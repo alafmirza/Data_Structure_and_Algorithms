1fd0a1b0eaa77a8f3c7e22ddc17611cf69f7aed6
Alaf Baig <mirzaalaf21@gmai.com>
2025-09-12 11:05:44 +0530

    array passing into methos,shallow copy

--- src/Arrays/passing_array.java
+++ src/Arrays/passing_array.java
@@ -1,4 +1,16 @@
 package Arrays;
 
 public class passing_array {
+    public static void main(String[] args) {
+        int[] x = {10,20,30,40,50};
+        x[0] = 100;
+        System.out.println(x[0]);
+
+        change(x);
+        System.out.println(x[0]);
+
+    }
+    public static void change(int[] x){
+        x[0]= 200;
+    }
 }
--- src/Arrays/shallow_deep_copy.java
+++ src/Arrays/shallow_deep_copy.java
@@ -1,4 +1,27 @@
 package Arrays;
 
-public class shalow_deep_copy {
+import java.util.Arrays;
+
+public class shallow_deep_copy {
+    public static void main(String[] args) {
+        int[] x = {10,20,30,40,50};
+
+        //shallow copy
+
+        int[] arr = x;
+        arr[0]=100;
+
+        //deep copy
+        int[] y = Arrays.copyOf(x,x.length);
+
+        y[0] = 500;
+
+
+        System.out.println(arr[0]);
+        System.out.println(y[0]);
+
+
+    }
+
+
 }
