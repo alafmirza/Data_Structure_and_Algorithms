00fb3b92b986e4e42b1298c37540039b51abf740
Alaf Baig <mirzaalaf21@gmai.com>
2025-09-16 12:40:20 +0530

    missing Element of array

---
+++ src/Arrays/missingElement.java
@@ -0,0 +1,16 @@
+package Arrays;
+
+public class missingElement {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6,8};
+        int n = 8;
+        int sum1 = n*(n+1)/2;
+        int sum2 = 0;
+        for (int i=0;i<arr.length;i++){
+             sum2 += arr[i];
+        }
+        System.out.println(sum1-sum2);
+
+    }
+
+}
