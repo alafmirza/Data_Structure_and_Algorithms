fd21b9e3d1ee9c7bdd2871a4fc88d7191768cee8
Alaf Baig <mirzaalaf21@gmai.com>
2025-09-16 13:32:53 +0530

    Missing Elelemt in array

--- src/Arrays/missingElement.java
+++ src/Arrays/missingElement.java
@@ -3,7 +3,7 @@ package Arrays;
 public class missingElement {
     public static void main(String[] args) {
         int[] arr = {1,2,3,4,5,6,8};
-        int n = 8;
+        int n = arr.length +1;
         int sum1 = n*(n+1)/2;
         int sum2 = 0;
         for (int i=0;i<arr.length;i++){
