07211e0c46bf5af5fa20c1f8bc127ed222a9c9b6
Alaf Baig <mirzaalaf21@gmai.com>
2025-09-24 09:10:42 +0530

    bubble sort worst case

--- src/Basic_sorting/Bubble_sort.java
+++ src/Basic_sorting/Bubble_sort.java
@@ -8,13 +8,32 @@ public class Bubble_sort {
     }
 
     public static void main(String[] args) {
-        int[] arr = {6, 7, 6, 5, 4, 1, 2, 10};
+        int[] arr = {9, 7, 6, 5, 4, 1, 18,2, 10};
         int n = arr.length;
 
         //bubble sort approach 1
 
+//        for(int i=0;i<n-1;i++) {
+//            for (int j = 0; j < n-1-i; j++) {  //count of lopp j
+//                if (arr[j] > arr[j + 1]) {
+//                    int temp = arr[j];
+//                    arr[j] = arr[j + 1];
+//                    arr[j + 1] = temp;
+//                }
+//            }
+//        }
+         // approach 2
+
         for(int i=0;i<n-1;i++) {
-            for (int j = 0; j < n-1-i; j++) {  //count of lopp j
+            boolean flag = true;
+            for(int j=0;j<n-1;j++){                // check array is sorted or not
+                if(arr[j] > arr[j+1]){
+                    flag = false;
+                    break;                          //if not sorted  move down code
+                }
+                if (flag) break;                     // if sort break 1 loop
+            }
+            for (int j = 0; j < n-1-i; j++) {  //
                 if (arr[j] > arr[j + 1]) {
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
@@ -23,5 +42,9 @@ public class Bubble_sort {
             }
         }
         print(arr);
+
+
+
+
     }
 }
