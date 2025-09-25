e72f1376a4ccfe175e6102e8ca5e18d2d2304d7c
Alaf Baig <mirzaalaf21@gmai.com>
2025-09-25 10:55:23 +0530

    Selection Sor

---
+++ src/Basic_sorting/Selection_sort.java
@@ -0,0 +1,26 @@
+package Basic_sorting;
+
+public class Selection_sort {
+    public static void main(String[] args) {
+        int[] arr = {6,-1,3,7,18,9,4};
+        int n = arr.length;
+        for(int i=0;i<n-1;i++){
+            int min = Integer.MAX_VALUE;  // minimum vbalue
+            int mindx = -1;   // minimum value index
+            for (int j=i;j<n;j++){
+                if(arr[j] < min){
+                    min=arr[j];
+                    mindx =j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[mindx];
+            arr[mindx] = temp;
+        }
+        for(int e : arr){
+            System.out.print(" "+e);
+        }
+    }
+
+}
+// time complexity O(n2)
