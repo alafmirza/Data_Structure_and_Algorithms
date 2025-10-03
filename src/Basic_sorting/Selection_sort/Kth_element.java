9edf4092c3d8fe7e240fd864b61ad5d583f1c883
Alaf Baig <mirzaalaf21@gmai.com>
2025-10-03 10:35:41 +0530

    Kth element of an array

---
+++ src/Basic_sorting/Selection_sort/Kth_element.java
@@ -0,0 +1,25 @@
+package Basic_sorting.Selection_sort;
+
+public class Kth_element {
+    public static void main(String[] args) {
+        int[] arr = {23,11,66,45,93,81,38};
+        int n = arr.length;
+        int k = 3;
+        for(int i=0;i<k;i++){
+            int min = Integer.MAX_VALUE;
+            int mindex = -1;
+            for(int j=i;j<k;j++){
+                if(arr[j]<min){
+                    min = arr[j];
+                    mindex = j;
+                }
+            }
+              int temp = arr[i];
+            arr[i] = arr[mindex];
+            arr[mindex] = temp;
+        }
+        System.out.println("Kth element is " +arr[k-1]);
+
+
+    }
+}
